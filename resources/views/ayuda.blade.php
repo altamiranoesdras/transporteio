2688c443546474a7761ecf13af39688ebac8ef6f
Angely Guevara <angely23gm@gmail.com>
2023-10-25 20:26:49 -0600

    Mas informacion sobre la empresa

--- resources/views/ayuda.blade.php
+++ resources/views/ayuda.blade.php
@@ -43,7 +43,21 @@
                         <h1>Datos De la Empresa</h1>
                         <p>Con más de 50 años, hemos evolucionado  
                         la manera de ofrecer y fabricar productos en madera tratada de alta calidad.</p>
-                        <h2></h2>
+                        <h2>NOSOTROS</h2>
+                        <p>Lignum es una empresa familiar con sede en Guatemala, con más de 50 años de experiencia en la industria de productos forestales,
+                         estamos integrados de forma vertical y cerramos el ciclo completo, desde la genética. </p>
+                         <p>siembra y aprovechamiento, industria y carpintería, comercialización, diseño y construcción de casas, pérgolas, decks y otras 
+                            edificaciones, con presencia en varios países de CA y Panamá</p>
+                     </p>
+
+                     <p>
+                        <h3>MISIÓN</h3>
+                        <p>Lograr nuestros objetivos por medio de esfuerzo, dedicación y un trabajo constante,
+                             para ofrecer productos y servicios forestales excelentes que satisfagan las necesidades de 
+                             nuestros clientes en el mercado, a través de un proceso sostenible con reforestación.</p>
+                             <h4>VISIÓN</h4>
+                             <p>Ser la empresa líder en Centroamérica de productos forestales sostenibles con el 
+                                medio ambiente y con la sociedad.</p>
                      </p>
                 </div>
             </div>
