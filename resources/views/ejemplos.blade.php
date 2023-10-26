a4ad11513d31459c711e6897da0ed4ca6f6b74f2
Antony Rios <antonycastaneda20@gmail.com>
2023-10-25 20:39:41 -0600

    Agregue otra imagen

--- resources/views/ejemplos.blade.php
+++ resources/views/ejemplos.blade.php
@@ -70,6 +70,14 @@
                                 </div>
                             </div>
                         </div>    
+                        <div class="col-md-4">
+                            <div class="card mb-4">
+                                <img src="{{asset('img/ejemplos/5.png')}}" class="card-img-top" alt="Imagen 3">
+                                <div class="card-body">
+                                    <h5 class="card-title">Ejemplo 5 </h5>
+                                </div>
+                            </div>
+                        </div>        
 
                             
 
